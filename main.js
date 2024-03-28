622f3fc5ceb6ba30daf96b08f4ea4f0922cfa790
ITBATMAN93 <96187465+ITBATMAN93@users.noreply.github.com>
2024-03-28 14:07:49 +0100

    commit

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ const myButton = document.querySelector('.my-button');
 // Add a click event listener to the button
 myButton.addEventListener('click', () => {
   // Log a message to the console when the button is clicked
-  console.log('Button was clicked!');
+  console.log('Conferma Avvenuta');
 });
