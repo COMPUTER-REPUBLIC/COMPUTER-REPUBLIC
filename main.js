a4d1b850a19d9086a2aeb6f64530a1bb553641bb
COMPUTER-REPUBLIC <96187465+COMPUTER-REPUBLIC@users.noreply.github.com>
2024-03-29 12:12:57 +0100

    commit e push

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// Select the button by its class
 const myButton = document.querySelector('.my-button');
-
-// Add a click event listener to the button
 myButton.addEventListener('click', () => {
-  // Log a message to the console when the button is clicked
   console.log('Conferma Avvenuta');
 });
